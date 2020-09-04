c4450580bb6528ffdeba48a909e6655f17debd9a
HajaSC <hajasc@protonmail.com>
2020-09-03 21:35:52 -0400

    Adds build

---
+++ build/precache-manifest.463ec32f7f8319b3816decc164abf37e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440d9de38ae0d37bb20991ffe8c978dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "391f12fc32d56465ad21",
+    "url": "/static/css/main.659a4c79.chunk.css"
+  },
+  {
+    "revision": "78cb79efd3473a58fa36",
+    "url": "/static/js/2.ffa911d6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ffa911d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "391f12fc32d56465ad21",
+    "url": "/static/js/main.761602a1.chunk.js"
+  },
+  {
+    "revision": "f59635ae36b1d8b2b3f5",
+    "url": "/static/js/runtime-main.68f972c2.js"
+  },
+  {
+    "revision": "9987fc5663ea3486dd81afc96918513f",
+    "url": "/static/media/star_wars_bg.9987fc56.jpg"
+  }
+]);
